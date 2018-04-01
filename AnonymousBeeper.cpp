56958fe9ee5666aa22eb391357ea9a097e9e8e21
JaminROCK <KingJamin@hotmail.com>
2018-03-31 20:53:33 -0600

    Update AnonymousBeeper.cpp

--- AnonymousBeeper.cpp
+++ AnonymousBeeper.cpp
@@ -4,16 +4,35 @@ using namespace std;
 
 int main()
 {
-	int z=10, b=10, c=10;
+	int z=10, d=10, e=10, q = 10;
 	cout << "Tell me a number: ", cin >> z; 
 
 	auto a = [&z](int x, int y)
 	{
 		z=100;
-		cout << "\079\tBEEP \bBOOP\a\007\t\rBEEPBOOP\007\008\069" << endl;
+		cout << "\n\079\tBEEP \bBOOP\a\007\t\rBEEPBOOP\007\008\069\n" << endl;
 		return x + y + z;
 	};
-	cout << a(b,c);
+	auto b = [=](int x, int y)
+	{
+		cout << "\n\079\tBEEP \bBOOP\a\007\t\rBEEPBOOP\007\008\069\n" << endl;
+		return x + y + z;
+	};
+	auto c = [&](int x, int y)
+	{
+		q = z;
+		cout << "\n\079\tBEEP \bBOOP\a\007\t\rBEEPBOOP\007\008\069\n" << endl;
+		return x + y + z;
+	};
+
+	cout << a(d,e);
+	cout << b(d,e);
+	cout << c(d,e);
+	if(q == z)
+	{
+		cout << "HELP COMPUTER" << endl;
+	}
+	
 
 	return 0;
 }
