31b98661336f0f741d2c7581a2a4db654de80f36
JaminROCK <KingJamin@hotmail.com>
2018-03-31 21:16:56 -0600

    Update AnonymousBeeper.cpp

--- AnonymousBeeper.cpp
+++ AnonymousBeeper.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
-
 int main()
 {
 	int z=10, d=10, e=10, q = 10;
+	vector<int> BEEP = {z, d, e, q};
 	cout << "Tell me a number: ", cin >> z; 
-
 	auto a = [&z](int x, int y)
 	{
 		z=100;
@@ -24,15 +24,14 @@ int main()
 		cout << "\b\b\n\079\tBEEP \bBOOP\e\007\t\rBEEPBOOP\007\008\069\n" << endl;
 		return x + y + z;
 	};
-
 	cout << a(d,e);
 	cout << b(d,e);
 	cout << c(d,e);
 	if(q == z)
 	{
-		cout << "HELP COMPUTER" << endl;
+		double QQ = 0; 
+		std::for_each(begin(BEEP),end(BEEP),[&](int x){QQ+=x;});
+		cout<<"HELP COMPUTER"<<QQ<<endl;
 	}
-	
-
 	return 0;
 }
